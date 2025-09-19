2ab4a161bdc3625f01b53d4155d909db4dfe7297
EtiMirzaei <atiyemirzaei@yahoo.com>
2025-09-19 20:25:01 +0430

    figma-w2

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-import "../index.css";
-// import "../css/main.css";
-        
-    var swiper = new Swiper(".mySwiper", {
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
